94906136f97209ad8000d27d557126928d590401
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-06 10:24:42 -0300

    feat: add custom types to styled components module

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,18 @@
+import 'styled-components';
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string;
+
+    colors: {
+      primary: string;
+      secundary: string;
+
+      purple: string;
+
+      background: string;
+      text: string;
+
+      border: string;
+    };
+  }
+}
