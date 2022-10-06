847edd929e454da472fcca551891c578930ea245
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-06 10:25:21 -0300

    feat: add theme provider to application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from "styled-components";
+import light from "../styles/themes/light";
+
+import GlobalStyle from "../styles/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={light}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
