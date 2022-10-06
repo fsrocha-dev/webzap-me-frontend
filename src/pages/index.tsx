6b3d81fb24766006760fdb6fdb20aefa4f62ef62
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-06 10:25:08 -0300

    feat: add head to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
-    <h1>Webzap</h1>
+    <>
+      <Head>
+        <title>WebZapMe | Rastreamento de leads via link do zap</title>
+      </Head>
+      <h1>Webzap</h1>
+    </>
   )
 }
 
