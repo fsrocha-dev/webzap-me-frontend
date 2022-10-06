e321bc4505e7246e35330ce0ee86ba59b67b8cb0
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-06 10:24:09 -0300

    feat: add light theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,22 @@
+import { lighten, shade } from 'polished';
+
+const light = {
+  title: 'light',
+
+  colors: {
+    primary: '#f9f9f9',
+    secundary: '#747474',
+
+    purple: '#646cff',
+
+    background: '#fff',
+    text: '#213647',
+
+    border: lighten(0.45, '#747474'),
+
+    link: '#747474',
+    linkHover: '#646cff'
+  }
+};
+
+export default light;
