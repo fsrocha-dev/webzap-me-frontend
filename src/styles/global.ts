e2025a83d2f3b3ba2ee5d916897b4cf7da2074c0
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-06 10:24:55 -0300

    feat: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+  }
+  body {
+    background: ${props => props.theme.colors.background};
+    color: ${props => props.theme.colors.text};
+  }
+  body html #root {
+    height: 100%;
+  }
+  body, input, textarea, select, button {
+    font: 400 1rem Poppins, sans-serif;
+  }
+  button {
+    cursor: pointer;
+  }
+  a{
+    text-decoration: none;
+    color: inherit;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+`;
