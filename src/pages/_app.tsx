b97b16dd506e9baed1d7aaec0eb8b9dd99884831
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-08 13:15:17 -0300

    feature: add components base to index app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from 'next/app';
 import { ThemeProvider } from "styled-components";
 import light from "../styles/themes/light";
 
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
 import GlobalStyle from "../styles/global";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={light}>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </ThemeProvider>
   )
 }
