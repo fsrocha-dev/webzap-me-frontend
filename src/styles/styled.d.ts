bc68981b5c252cbf9ef3ede4995d51520e6e75be
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-08 13:12:47 -0300

    fix: styles types on light theme

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,14 +3,19 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     title: string;
 
+    defaultRadiusBorder: string;
+
     colors: {
       primary: string;
       secundary: string;
 
-      purple: string;
+      green: string;
 
       background: string;
       text: string;
+
+      link: string;
+      linkHover: string;
 
       border: string;
     };
--- src/styles/themes/light.ts
+++ src/styles/themes/light.ts
@@ -2,20 +2,21 @@ import { lighten, shade } from 'polished';
 
 const light = {
   title: 'light',
+  defaultRadiusBorder: '3rem',
 
   colors: {
-    primary: '#f9f9f9',
+    primary: '#f7f7f7',
     secundary: '#747474',
 
-    purple: '#646cff',
+    green: '#36C9B9',
 
     background: '#fff',
     text: '#213647',
 
     border: lighten(0.45, '#747474'),
 
-    link: '#747474',
-    linkHover: '#646cff'
+    link: '#213647',
+    linkHover: '#36C9B9'
   }
 };
 
