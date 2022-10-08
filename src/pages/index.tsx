80a514518ebe3bf5eeda60640d55e12bae1458be
Frank Rocha <contato.frankrocha@gmail.com>
2022-10-08 13:15:03 -0300

    feature: add home base structure components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { RiWhatsappLine } from 'react-icons/ri'
+import { LinkGenerator } from '../components/LinkGenerator'
+import { CallActionButton, HomeHero } from './styles/home'
 
 const Home: NextPage = () => {
   return (
@@ -7,7 +10,14 @@ const Home: NextPage = () => {
       <Head>
         <title>WebZapMe | Rastreamento de leads via link do zap</title>
       </Head>
-      <h1>Webzap</h1>
+      <HomeHero>
+        <span>Dê um up no seu negócio com o WebZapMe</span>
+        <h1>Gere um link grátis do WhatsApp <RiWhatsappLine /> </h1>
+        <CallActionButton>
+          Gerar link do Zap
+        </CallActionButton>
+      </HomeHero>
+      <LinkGenerator />
     </>
   )
 }
