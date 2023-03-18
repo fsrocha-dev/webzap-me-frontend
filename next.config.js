445a42e88a7faf12435f94244b9fdfcf69869c02
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-18 16:28:18 -0300

    build: update packages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    // Enables the styled-components SWC transform
-    styledComponents: true
-  }
+	reactStrictMode: false,
+	swcMinify: true,
+	compiler: {
+		// Enables the styled-components SWC transform
+		styledComponents: true
+	}
 };
 
 module.exports = nextConfig;
