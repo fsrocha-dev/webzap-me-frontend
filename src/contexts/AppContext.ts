d24e05f0934f4f30c350bb41a18b317de7d3af0d
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-19 10:47:54 -0300

    chore: configure app context data

--- src/contexts/AppContext.ts
+++ src/contexts/AppContext.ts
@@ -1,6 +1,19 @@
 import React from 'react';
 import { AppContextType } from '../@types/appContext';
 
-const AppContext = React.createContext<AppContextType | null>(null);
+const defaultState = {
+  LoginModalIsOpen: null,
+  setLoginModelIsOpen: () => {},
+  registermodalIsOpen: null,
+  setRegisterModalIsOpen: () => {},
+  newLink: null,
+  setNewLink: () => {},
+  qrCodeModalIsOpen: null,
+  setQrCodeModalIsOpen: () => {},
+  qrCode: null,
+  setQrCode: () => {}
+};
+
+const AppContext = React.createContext<AppContextType>(defaultState);
 
 export default AppContext;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,60 @@
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { ThemeProvider } from "styled-components";
-import light from "../styles/themes/light";
-
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import AppContext from '../contexts/AppContext';
+import UserContext from '../contexts/UserContext';
 import GlobalStyle from '../styles/global';
+import light from "../styles/themes/light";
+import { getLocalStorageValue } from "./../utils/LocalStorage";
 
+interface ILinkState {
+    phone_number: number;
+    message: string;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [userLoggedIn, setUserLoggedIn] = useState(null);
+  const [LoginModalIsOpen, setLoginModelIsOpen] = useState(false);
+  const [registermodalIsOpen, setRegisterModalIsOpen] = useState(false);
+  const [qrCodeModalIsOpen, setQrCodeModalIsOpen] = useState(false);
+  const [newLink, setNewLink] = useState<ILinkState | undefined>(undefined);
+  const [qrCode, setQrCode] = useState<string | null>(null);
+
+  useEffect(() => {
+    const checkLoggedUser = getLocalStorageValue('loggedUser')
+    checkLoggedUser ? setUserLoggedIn(checkLoggedUser) : null
+  }, [])
+
   return (
-    <AppContext.Provider value={{}}>
-      <ThemeProvider theme={light}>
-        <GlobalStyle />
-        <Header />
-        <Component {...pageProps} />
-        <Footer />
-      </ThemeProvider>
+    <AppContext.Provider value={
+      {
+        LoginModalIsOpen,
+        setLoginModelIsOpen,
+        registermodalIsOpen,
+        setRegisterModalIsOpen,
+        newLink,
+        setNewLink,
+        qrCodeModalIsOpen,
+        setQrCodeModalIsOpen,
+        qrCode, 
+        setQrCode
+      }}>
+      <UserContext.Provider value={{
+        userLoggedIn,
+        setUserLoggedIn
+      }}>
+        <ThemeProvider theme={light}>
+          <GlobalStyle />
+          <Header />
+          <Component {...pageProps} />
+          <Footer />
+        </ThemeProvider>
+      </UserContext.Provider>
+      <ToastContainer />
     </AppContext.Provider>
   )
 }
