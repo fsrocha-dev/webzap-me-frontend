9e3fc1eb81717cb60e67473137f235dcf9bbbf58
Frank Rocha <contato.frankrocha@gmail.com>
2023-03-19 10:48:16 -0300

    feat: link generator component

--- src/components/LinkGenerator/index.tsx
+++ src/components/LinkGenerator/index.tsx
@@ -1,9 +1,196 @@
-import { LinkGeneratorContent } from './styles'
+import axios from 'axios';
+import dynamic from 'next/dynamic';
+import 'Devices.css/assets/devices.min.css';
+import Image from "next/image";
+import { LegacyRef, useContext, useEffect, useState } from 'react';
+import { MdSend } from 'react-icons/md';
+import { RiArrowDropRightLine } from 'react-icons/ri';
+import { toast } from 'react-toastify';
+import MaskedInput from 'react-text-mask'
+import AppContext from '../../contexts/AppContext';
+import { getLocalStorageValue } from '../../utils/LocalStorage';
+import whatsappLink from '../../utils/WhatsappLink';
+import { QrCode } from '../Modals/QrCode';
+import {
+	DevicePhone, DisplayedMessage, EmojiButton, GenerateButton, LinkGeneratorContainer,
+	LinkGeneratorContent,
+	LinkGeneratorForm,
+	LinkGeneratorHeader
+} from './styles';
+import { EmojiStyle } from 'emoji-picker-react';
+const Picker = dynamic(
+	() => {
+		return import('emoji-picker-react');
+	},
+	{ ssr: false }
+);
 
-export function LinkGenerator() {
-  return (
-      <LinkGeneratorContent>
-        <span>Seção</span>
-      </LinkGeneratorContent>
-  )
+interface AnchorProps {
+	LinkGeneratorAnchor: LegacyRef<HTMLHeadingElement>
+}
+
+interface IFormLink {
+	phone_number: string | null;
+	custom_message: string;
+}
+
+export function LinkGenerator(props: AnchorProps) {
+
+	const { LinkGeneratorAnchor } = props
+	const { setQrCodeModalIsOpen, setQrCode, setLoginModelIsOpen } = useContext(AppContext);
+	const [formLinkValue, setFormLinkValue] = useState<IFormLink>({ phone_number: null, custom_message: '' })
+	const [showBubble, setShowBubble] = useState(false)
+	const [message, setMessage] = useState<string | null>(null)
+	const [emojiPickerIsOpen, setEmojiPickerIsOpen] = useState<boolean>(false)
+
+	useEffect(() => {
+		if (formLinkValue.custom_message) {
+			const secondsDelay = 4
+			let timerBubbleMessage = setTimeout(() => {
+				setShowBubble(true)
+			}, secondsDelay * 1000);
+
+			return () => {
+				clearTimeout(timerBubbleMessage);
+			};
+		}
+	}, [formLinkValue.custom_message])
+
+	const formatCellNumber = (celNumber: string) => {
+		return celNumber.replace(/[^\w\s]/gi, '').replaceAll(' ', '')
+	}
+
+	const generateLink = async (event: React.FormEvent) => {
+		event.preventDefault();
+		const checkLoggedUser = getLocalStorageValue('loggedUser')
+
+		if (!checkLoggedUser) {
+			toast("Você deve estar logado para criar um link")
+			setLoginModelIsOpen(true)
+		} else {
+
+			try {
+				const URL = 'http://localhost:5001/links'
+				const headers = { headers: { "Authorization": `Bearer ${checkLoggedUser.token}` } }
+				const zapLink = whatsappLink(formLinkValue)
+				const { data } = await axios.post(URL, { ...formLinkValue, whatsapp_link: zapLink }, headers)
+
+				setQrCode(data.ref)
+				setQrCodeModalIsOpen(true)
+
+			} catch (error: any) {
+				setMessage(error.response.data)
+			}
+		}
+	}
+
+	const openEmojiPicker = async (e) => {
+		e.preventDefault()
+		setEmojiPickerIsOpen(!emojiPickerIsOpen)
+	}
+
+	const addEmojiOnTextAreaMessage = async (e) => {
+		const emoji = ` ${e.emoji}`
+		let { custom_message: message } = formLinkValue
+		setFormLinkValue({ ...formLinkValue, custom_message: message += emoji })
+		setEmojiPickerIsOpen(false)
+	}
+
+	return (
+		<LinkGeneratorContainer>
+			<LinkGeneratorHeader>
+				<h2 ref={LinkGeneratorAnchor}>
+					Crie gratuitamente o seu link abaixo!
+				</h2>
+			</LinkGeneratorHeader>
+			<LinkGeneratorContent>
+				<LinkGeneratorForm>
+					<label>
+						Digite o seu número de WhatsApp
+						<MaskedInput
+							mask={[
+								'(', /[1-9]/, /\d/, ')',
+								' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+							guide={false}
+							placeholder='Seu número de telefone'
+							onChange={(e) => setFormLinkValue({ ...formLinkValue, phone_number: formatCellNumber(e.target.value) })}
+							value={formLinkValue.phone_number ?? ''}
+						/>
+						<small>Obs: Verifique se inseriu o código de área.</small>
+					</label>
+					<label>
+						Digite sua mensagem personalizada
+						<textarea
+							rows={4}
+							onChange={(e) => setFormLinkValue({ ...formLinkValue, custom_message: e.target.value })}
+							value={formLinkValue.custom_message ?? ''}
+							placeholder='Mensagem personalizada que os usuários enviarão para seu zap' />
+						<small>Ex: Olá, estou muito interessado em seu produto! Pode falar agora?</small>
+						<EmojiButton onClick={openEmojiPicker}>
+							🤩 Adiconar emoji
+						</EmojiButton>
+						<span style={{ position: "absolute", marginTop: "-80px", marginLeft: "130px" }}>
+							{emojiPickerIsOpen
+								&&
+								<Picker
+									emojiStyle={EmojiStyle.TWITTER}
+									onEmojiClick={(e) => addEmojiOnTextAreaMessage(e)}
+								/>}
+						</span>
+					</label>
+					<div className='error-message'>
+						{message}
+					</div>
+					<GenerateButton onClick={generateLink}>
+						Criar meu link
+					</GenerateButton>
+				</LinkGeneratorForm>
+				<DisplayedMessage>
+					<RiArrowDropRightLine />
+					<span>
+						Veja ao lado como será exibido
+					</span>
+				</DisplayedMessage>
+				<DevicePhone>
+					<div className="marvel-device iphone8 silver">
+						<div className="top-bar"></div>
+						<div className="sleep"></div>
+						<div className="volume"></div>
+						<div className="camera"></div>
+						<div className="sensor"></div>
+						<div className="speaker"></div>
+						<div className="screen">
+							<div className="screen-header" >
+								<Image
+									src="/images/profile-pic.svg"
+									alt="Imagem do usuário"
+									layout="fixed"
+									width={35}
+									height={35} />
+								<span>+55 {formLinkValue.phone_number}</span>
+							</div>
+							<div className="screen-content">
+								{showBubble === true &&
+									<div className="bubble-message">
+										{formLinkValue.custom_message}
+									</div>
+								}
+							</div>
+							<div className="screen-footer">
+								<div className="footer-input">
+									{formLinkValue.custom_message}
+								</div>
+								<div className="footer-send">
+									<MdSend />
+								</div>
+							</div>
+						</div>
+						<div className="home"></div>
+						<div className="bottom-bar"></div>
+					</div>
+				</DevicePhone>
+			</LinkGeneratorContent>
+			<QrCode />
+		</LinkGeneratorContainer>
+	)
 }
--- src/components/LinkGenerator/styles.ts
+++ src/components/LinkGenerator/styles.ts
@@ -1,12 +1,214 @@
+import { lighten, shade } from 'polished';
 import styled from 'styled-components';
 
-export const LinkGeneratorContent = styled.section`
+export const LinkGeneratorContainer = styled.section`
+  min-height: calc(550px - 4rem);
+  padding: 2rem 2rem;
+
+  display: block;
+`;
+
+export const LinkGeneratorHeader = styled.div`
   max-width: 1120px;
-  height: calc(100vh - 4rem);
+  margin: 0 auto;
+  padding: 0 2rem;
+  h2 {
+    font-size: 2rem;
+  }
+`;
+
+export const LinkGeneratorContent = styled.div`
+  max-width: 1120px;
   margin: 0 auto;
   padding: 0 2rem;
 
   display: flex;
+  justify-content: space-around;
+`;
+
+export const LinkGeneratorForm = styled.form`
+  max-width: 400px;
+  min-height: 495px;
+  margin: 2rem 0;
+  padding: 1rem 0.6rem;
+
+  border: 1px solid ${props => shade(0.1, props.theme.colors.primary)};
+  border-radius: 8px;
+
+  display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: space-between;
+
+  small {
+    font-weight: normal;
+    font-size: 11px;
+    color: ${props => shade(0.7, props.theme.colors.gray_200)};
+  }
+
+  label {
+    font-weight: bold;
+    margin: 0.8rem 0;
+
+    display: block;
+  }
+
+  .error-message {
+    color: ${props => props.theme.colors.red};
+    font-size: 12px;
+  }
+`;
+
+export const DisplayedMessage = styled.div`
+  font-size: 12px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  svg:first-child {
+    font-size: 3rem;
+  }
+`;
+
+export const DevicePhone = styled.div`
+  height: 100%;
+
+  .marvel-device {
+    width: 270px;
+    height: 25rem;
+    z-index: -1;
+
+    .sensor {
+      left: 86px;
+    }
+
+    .screen {
+      background-image: url('./images/wa-background.png');
+      background-size: cover;
+      background-repeat: no-repeat;
+
+      display: flex;
+      flex-direction: column;
+    }
+
+    .screen-content {
+      display: flex;
+      align-items: flex-end;
+      justify-content: right;
+
+      padding: 0.5rem 0.5rem;
+      flex: auto;
+
+      .bubble-message {
+        max-width: 85%;
+        word-wrap: break-word;
+        background: #dcf8c6;
+        border-radius: 16px;
+        padding: 0.7rem 0.5rem;
+
+        font-size: 14px;
+      }
+    }
+
+    .screen-header {
+      background-color: #128c7e;
+      height: 3rem;
+      padding: 0.3rem 1rem;
+
+      display: flex;
+      flex-direction: row;
+      gap: 10px;
+
+      img {
+        border-radius: 50%;
+      }
+      span {
+        color: #ffffff;
+        display: flex;
+        align-items: center;
+      }
+    }
+
+    .screen-footer {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+
+      padding: 0.5rem 0.5rem;
+      .footer-input {
+        background-color: #ffffff;
+        border-radius: 16px;
+        padding: 0.4rem 1rem;
+
+        font-size: 14px;
+
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        width: 210px;
+      }
+      .footer-send {
+        width: 35px;
+        height: 35px;
+        background-color: #128c7e;
+        border-radius: 50%;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        font-size: 22px;
+        color: #ffffff;
+      }
+    }
+  }
+`;
+
+export const GenerateButton = styled.button`
+  height: 3.2rem;
+  width: 100%;
+  border-radius: ${props => props.theme.defaultRadiusBorder};
+  background: ${props => props.theme.colors.linkHover};
+  border: 0;
+  margin-top: 20px;
+  padding: 0 1.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: white;
+  font-weight: normal;
+
+  svg {
+    width: 18px;
+    height: 18px;
+  }
+
+  svg:first-child {
+    margin-right: 1rem;
+  }
+
+  transition: 0.2s;
+
+  &:hover {
+    background: ${props => lighten(0.1, props.theme.colors.linkHover)};
+  }
+`;
+
+export const EmojiButton = styled.button`
+  height: 1.5rem;
+  background: none;
+  border: 0;
+  margin: 10px 0;
+  padding: 0.2rem 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  color: ${props => props.theme.colors.secundary};
+  font-weight: normal;
+  font-size: 14px;
 `;
